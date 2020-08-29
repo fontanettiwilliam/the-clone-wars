7ee196dcd5c12448d75a5cda3b3509f7898f901b
fontanettiwilliam <williampriester@gmail.com>
2020-08-29 09:23:04 -0300

    Sidebar Options

--- spotify-clone/src/Sidebar.js
+++ spotify-clone/src/Sidebar.js
@@ -2,14 +2,22 @@ import React from "react";
 import "./Sidebar.css";
 import Logo from "./asset/logo/spotify_white.png";
 import SidebarOption from "./SidebarOption";
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import LibraryMusicOutlinedIcon from "@material-ui/icons/LibraryMusicOutlined";
 function Sidebar() {
   return (
     <div className="sidebar">
       <img className="sidebar_logo" src={Logo} alt="sidebar-logo" />
 
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search" />
-      <SidebarOption title="Your Library" />
+      <SidebarOption title="Home" Icon={HomeOutlinedIcon} />
+      <SidebarOption title="Search" Icon={SearchOutlinedIcon} />
+      <SidebarOption title="Your Library" Icon={LibraryMusicOutlinedIcon} />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
+      <SidebarOption title="Rock" />
+      <SidebarOption title="Hip Hop" />
+      <SidebarOption title="Classic" />
     </div>
   );
 }
