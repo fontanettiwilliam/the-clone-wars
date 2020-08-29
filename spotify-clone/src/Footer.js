72c17ffb5aec2d39ff12c529bb041377e8991434
fontanettiwilliam <williampriester@gmail.com>
2020-08-29 10:01:20 -0300

    Footer > PlaySong

--- spotify-clone/src/Footer.js
+++ spotify-clone/src/Footer.js
@@ -1,11 +1,53 @@
 import React from "react";
 import "./Footer.css";
+
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import PlayCircleOutlineOutlinedIcon from "@material-ui/icons/PlayCircleOutlineOutlined";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import RepeatIcon from "@material-ui/icons/Repeat";
+
+import { Grid, Slider } from "@material-ui/core";
+
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer_left"></div>
-      <div className="footer_center"></div>
-      <div className="footer_right"></div>
+      <div className="footer_left">
+        <img
+          className="footer_albumCover"
+          src="https://upload.wikimedia.org/wikipedia/pt/c/c1/The_Weeknd_-_After_Hours.png"
+          alt="album_cover"
+        />
+        <div className="footer_songInfo">
+          <h4>Blinding Lights</h4>
+          <p>The Weeknd</p>
+        </div>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineOutlinedIcon
+          fontSize="large"
+          className="footer_icon"
+        />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
